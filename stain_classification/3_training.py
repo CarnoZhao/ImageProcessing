d956342add2745ab33e060e22ca15d965562e358
CarnoZhao <273806108@qq.com>
2019-10-21 20:53:50 +0800

    add immediate print

--- stain_classification/3_training.py
+++ stain_classification/3_training.py
@@ -6,32 +6,31 @@ import torchvision
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import functions
 
-def dense_net_model(loader, decay, numIters, lr, preTrain, focal, weighted, smoothing, gamma, K):
+def dense_net_model(loaders, decay, numIters, lr, preTrain, focal, loss_weights, smoothing, gamma, K):
 
     ## Pre-Train ##
+    loader = loaders['train']
     if preTrain:
         net = torchvision.models.densenet121(pretrained = preTrain)
         for p in net.parameters():
             p.requires_grad = False
-        for p in net.features.denseblock4.parameters():
-            p.requires_grad = True
+        # for p in net.features.denseblock4.parameters():
+        #     p.requires_grad = True
         net.classifier = torch.nn.Linear(in_features = 1024, out_features = K, bias = True)
     else:
         net = torchvision.models.densenet121(num_classes = K)
     net.to('cuda')
 
     ## weight ##
-    if weighted:
-        # jizhi: 1850, tumor: 5798, tumorln: 521, huaisi: 337
-        t = np.array([1850, 5798, 521, 337])
-        t = (1 / t) / np.sum(1 / t)
-        weight = torch.FloatTensor(t).to('cuda')
-    else:
-        weight = None
+    if loss_weights is not None:
+        class_to_idx = loader.dataset.class_to_idx
+        weights = [0 for _ in range(K)]
+        for key in class_to_idx:
+            weights[class_to_idx[key]] = loss_weights[key]
 
     ## loss-opt ##
     # loss = functions.losses.LabelSmoothingFocalLoss(classes = K, smoothing = smoothing, weight = weight, focal = focal, gamma = gamma)
-    loss = functions.losses.Loss(K = K, focal = focal, gamma = gamma, smoothing = smoothing, weights = weight)
+    loss = functions.losses.Loss(K = K, focal = focal, gamma = gamma, smoothing = smoothing, weights = loss_weights)
     optimizer = torch.optim.Adamax(net.parameters(), lr = lr)
 
     ## iterating ##
@@ -48,15 +47,21 @@ def dense_net_model(loader, decay, numIters, lr, preTrain, focal, weighted, smoo
             cost.backward()
             costs += float(cost)
             optimizer.step()
-        if iteration % 5 == 0:
+        if iteration % 3 == 0:
             print("Cost after iteration %d: %.3f" % (iteration, costs))
+            _, testYhat, testaccu = functions.evaluation.predict(net, loaders['test'], K)
+            _, trainYhat, trainaccu = functions.evaluation.predict(net, loader, K)
+            print("Accuracy after iteration %d: train %.3f; test %.3f" % (iteration, trainaccu, testaccu))
+            print("Yhat sample in test:")
+            print(testYhat)
+            print("Yhat sample in train:")
+            print(trainYhat)
     return net
 
-def main(batch_size, num_batch, type_weight, decay, numIters, lr, preTrain, focal, weighted, smoothing, gamma, K):
+def main(batch_size, num_batch, type_weight, decay, numIters, lr, preTrain, focal, loss_weights, smoothing, gamma, K):
     path = "/wangshuo/zhaox/ImageProcessing/stain_classification/_data/cutted"
     loaders = functions.data.forceRatioLoader(path, batch_size, num_batch, type_weight)
-    trainloader = loaders['train']
-    net = dense_net_model(trainloader, decay, numIters, lr, preTrain, focal, weighted, smoothing, gamma, K)
+    net = dense_net_model(loaders, decay, numIters, lr, preTrain, focal, loss_weights, smoothing, gamma, K)
     torch.save(net, modelpath)
     functions.evaluation.printplot(net, loaders, K, plotpath, matpath)
 
@@ -74,20 +79,29 @@ type_weight = {
     "huaisi": 1
     }
 
+loss_weights = {
+    "jizhi": 0,
+    "tumor": 0,
+    "tumorln": 0,
+    "huaisi": 0
+}
+
 params = {
          "numIters":    20,
        "batch_size":    32,
-        "num_batch":    150,
+        "num_batch":    100,
          "preTrain":    True,
             "focal":    True,
-         "weighted":    False,
+     "loss_weights":    None,
             "decay":    True,
-               "lr":    0.001,
-        "smoothing":    0.01,
+               "lr":    0.0001,
+        "smoothing":    0.001,
             "gamma":    2,
                 "K":    4,
       "type_weight":    type_weight,
     }
+
+# {'huaisi': 0, 'jizhi': 1, 'tumor': 2, 'tumorln': 3}
 
 for key, value in params.items():
     print("{:<10s}{:>10s}".format(key, str(value)))
