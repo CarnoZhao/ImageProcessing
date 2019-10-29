032a575dc5cdf9eeade3f9a90d7b2a43665ce178
CarnoZhao <273806108@qq.com>
2019-10-29 15:39:05 +0800

    add my survloss

--- survival_analysis/2_train_fc.py
+++ survival_analysis/2_train_fc.py
@@ -1,12 +1,41 @@
-import torch
-import h5py
 import sys
+import h5py
+import torch
 import numpy as np
 import pandas as pd
-sys.path.insert(1, "/wangshuo/zhaox/ImageProcessing/survival_analysis/_ref")
-from DeepsurvLoss import SurvLoss
 from torch.utils.data import Dataset, DataLoader
 from lifelines.utils import concordance_index
+
+class SurvLoss(torch.nn.Module):
+    def __init__(self):
+        super(SurvLoss, self).__init__()
+        pass
+
+    def forward(self, Yhat, Y):
+        Yhat = torch.squeeze(Yhat)
+        Y = torch.squeeze(Y)
+        order = torch.argsort(Y)
+        Y = Y[order]
+        Yhat = Yhat[order]
+        E = (Y > 0).float()
+        T = torch.abs(Y)
+        Yhr = torch.log(torch.cumsum(torch.exp(Yhat), dim = 0))
+        obs = torch.sum(E)
+        Es = torch.zeros_like(E)
+        Yhrs = torch.zeros_like(Yhr)
+        j = 0
+        for i in range(1, len(T)):
+            if T[i] != T[i - 1]:
+                Es[i - 1] = torch.sum(E[j:i])
+                Yhrs[i - 1] = torch.max(Yhr[j:i])
+                j = i
+        Es[-1] = torch.sum(E[j:])
+        Yhrs[-1] = torch.max(Yhr[j:])
+        loss2 = torch.sum(torch.mul(Es, Yhrs))
+        loss1 = torch.sum(torch.mul(Yhat, E))
+        loss = torch.div(torch.sub(loss2, loss1), obs)
+        return loss
+        
 
 class Datasets(Dataset):
     def __init__(self, pats, label, hosi, istrain):
@@ -28,16 +57,14 @@ class Datasets(Dataset):
 class Net(torch.nn.Module):
     def __init__(self, layers, p):
         super(Net, self).__init__()
-        self.dr1 = torch.nn.Dropout(p)
         self.fc1 = torch.nn.Linear(in_features = 512, out_features = layers[0], bias = True)
-        self.dr2 = torch.nn.Dropout(p)
+        self.dr1 = torch.nn.Dropout(p)
         self.fc2 = torch.nn.Linear(in_features = layers[0], out_features = 1, bias = True)
 
     def forward(self, x):
-        #x = self.dr1(x)
         x = self.fc1(x)
         x = torch.nn.Tanh()(x)
-        x = self.dr2(x)
+        x = self.dr1(x)
         x = self.fc2(x)
         x = torch.nn.Tanh()(x)
         return x
@@ -48,7 +75,7 @@ def print_to_out(*args):
         f.write('\n')
 
 def load_data(h5path, csvpath):
-    h5 = h5py.File(h5path)
+    h5 = h5py.File(h5path, 'r')
     csv = pd.read_csv(csvpath)
     num_to_label = {csv.number[i]: csv.time[i] * 30 * (1 if csv.event[i] else -1) for i in range(len(csv))}
     pats = np.array(list(set(csv.number) & set(h5['patnum'])))
@@ -60,54 +87,67 @@ def load_data(h5path, csvpath):
     return pats, label, data, hosi
 
 def main(h5path, csvpath, lr, epochs, batch_size):
+    
+    ## load_data
     pats, label, data, hosi = load_data(h5path, csvpath)
+
+    ## laoder
     traindataset = Datasets(pats, label, hosi, True)
     trainloader = DataLoader(traindataset, batch_size = batch_size, shuffle = True)
+    # trainloader = DataLoader(traindataset, batch_size = len(traindataset), shuffle = True)
     testdataset = Datasets(pats, label, hosi, False)
     testloader = DataLoader(testdataset)
-    net = Net([256], 0.5).cuda()
+
+    ## pre parameter
+    net = Net([128], 0).cuda()
     loss = SurvLoss()
-    opt = torch.optim.Adamax(net.parameters(), lr = lr)
+    # opt = torch.optim.Adamax(net.parameters(), lr = lr)
+    opt = torch.optim.SGD(net.parameters(), lr = lr, momentum = 0.9, nesterov = True)
+
+    ## iteration
     for i in range(epochs):
         costs = 0
         for pats, y in trainloader:
-            order = torch.argsort(-torch.abs(torch.squeeze(y)))
-            y = y[order]
-            pats = pats[order]
+            x = torch.zeros((len(pats), 512)).cuda()
+            y = y.cuda()
+
+            # select instance
             net.eval()
-            x = []
-            for p in pats:
+            for j, p in enumerate(pats):
                 pdata = data[int(p)]
                 pyhat = net(torch.FloatTensor(pdata).cuda())
                 maxidx = torch.argmax(pyhat)
-                x.append(pdata[maxidx])
-            x = torch.FloatTensor(np.array(x)).cuda()
+                x[j] = torch.Tensor(pdata[maxidx])
+
+            # train
             net.train()
-            y = y.cuda()
             yhat = net(x)
             opt.zero_grad()
             cost = loss(yhat, y)
             cost.backward()
             costs += float(cost)
             opt.step()
-        if i % 10 == 0:
-            #print("%d | costs: %.3f" % (i, costs))            
+        if i % 10 == 0:          
             print_to_out("%d | costs: %.3f" % (i, costs))
+
+    ## evalu
     with torch.no_grad():
         for loader in [trainloader, testloader]:
-            Y = []
-            Yhat = []
+            Y = np.zeros(len(loader.dataset))
+            Yhat = np.zeros(len(loader.dataset))
+            i = 0
             for pats, y in loader:
-                Y.extend([yi for yi in y])
+                Y[i: i + len(y)] = y
                 for p in pats:
                     pdata = data[int(p)]
                     pyhat = net(torch.FloatTensor(pdata).cuda())
                     preds = torch.max(pyhat, dim = 0).values
-                    Yhat.append(float(preds))
-            Y = np.array(Y)
-            Yhat = np.array(Yhat)
-            ci = concordance_index(np.abs(Y), -1 * np.array(Yhat), np.where(Y > 0, 1, -1))
+                    Yhat[i] = float(preds)
+                    i += 1
+            ci = concordance_index(np.abs(Y), -1 * Yhat, np.sign(Y))
             print_to_out("ci = %.3f" % ci)
+
+    ## save
     torch.save(net, modelpath)
     
 
@@ -116,10 +156,10 @@ if __name__ == "__main__":
     modelpath, plotpath, matpath, outfile = sys.argv[1:5]
 
     params = {
-        "h5path": "/wangshuo/zhaox/ImageProcessing/survival_analysis/_data/computed_data.h5",
-        "csvpath": "/wangshuo/zhaox/ImageProcessing/survival_analysis/_data/merged.csv",
-        "lr": 1e-6,
+        "h5path": "/home/tongxueqing/zhao/ImageProcessing/survival_analysis/_data/computed_data.h5",
+        "csvpath": "/home/tongxueqing/zhao/ImageProcessing/survival_analysis/_data/merged.csv",
+        "lr": 1e-4,
         "epochs": 100,
-        "batch_size": 32,
+        "batch_size": 64,
     }
     main(**params)
