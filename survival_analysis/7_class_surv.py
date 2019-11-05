6198409ea786ce6058552a2d11148a99b262e70a
CarnoZhao <273806108@qq.com>
2019-11-05 21:27:16 +0800

    remove redundent codes, support kth-fold train

--- survival_analysis/7_class_surv.py
+++ survival_analysis/7_class_surv.py
@@ -22,7 +22,6 @@ if os.path.exists("/wangshuo/zhaox"):
 else:
     root = "/home/tongxueqing/zhao"
     torch.nn.Module.dump_patches = True
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 
 def print_to_out(*args):
     with open(outfile, 'a') as f:
@@ -55,24 +54,20 @@ class SurvLoss(torch.nn.Module):
         return loss
 
 class SurvDataset(torch.utils.data.Dataset):
-    def __init__(self, nameidx, set_pat, label):
+    def __init__(self, pat, label):
         super(SurvDataset, self).__init__()
-        if isinstance(nameidx, list):
-            pat = np.bitwise_or(*(set_pat == nidx for nidx in nameidx))
-        else:
-            pat = set_pat == nameidx
-        self.index = np.array(list(range(len(pat))))[pat]
+        self.pat = pat
         self.label = label
-        self.length = len(self.index)
+        self.length = len(self.pat)
 
     def __getitem__(self, i):
-        return self.index[i], self.label[self.index[i]]
+        return self.pat[i], self.label[self.pat[i]]
 
     def __len__(self):
         return self.length
       
 class Data(object):
-    def __init__(self, h5path):
+    def __init__(self, h5path, fold = 4, k = 0):
         h5 = h5py.File(h5path, 'r')
         self.set_pat = h5['set_pat'][:]
         self.pat_fig = h5['pat_fig'][:]
@@ -81,48 +76,34 @@ class Data(object):
         self.data = h5['data']
         self.postdata = h5['postdata'][:]
         self.names = ['train', 'val', 'test']
+        self.fold = fold
+        self.k = k
 
-    def load(self, batch_size, ifcombine = False):
-        if ifcombine:
-            datasets = {
-                'train': SurvDataset([0, 1], self.set_pat, self.label),
-                "test": SurvDataset(2, self.set_pat, self.label)
-            }
-        else:
-            datasets = {name: SurvDataset(i, self.set_pat, self.label) for i, name in enumerate(self.names)}
-        loaders = {name: DataLoader(datasets[name], batch_size = batch_size if name == 'train' else 1, shuffle = name == 'train') for name in datasets}
-        mapdic = {i: np.array(list(range(len(self.pat_fig[i]))))[self.pat_fig[i]] for i in range(len(self.pat_fig))}
+    def __datasets_creater(self):
+        train_val = np.arange(len(self.set_pat))[self.set_pat == 0]
+        inf = int(self.k * len(train_val) / self.fold)
+        sup = int((self.k + 1) * len(train_val) / self.fold)
+        validx = np.arange(len(train_val))
+        validx = np.bitwise_and(np.greater_equal(validx, inf), np.less(validx, sup))
+        train = train_val[np.bitwise_not(validx)]
+        val = train_val[validx]
+        test = np.arange(len(self.set_pat))[self.set_pat == 1]
+        datasets = {}
+        datasets['train'] = SurvDataset(train, self.label)
+        datasets['val'] = SurvDataset(val, self.label)
+        datasets['test'] = SurvDataset(test, self.label)
+        self.k += 1
+        return datasets
+
+    def load(self, batch_size, k = None):
+        if k is not None: self.k = k
+        datasets = self.__datasets_creater()
+        loaders = {name: DataLoader(datasets[name], batch_size = batch_size if name == 'train' else 1, shuffle = name == 'train', drop_last = name == 'train') for name in self.names}
+        mapdic = {i: np.arange(len(self.pat_fig[i]))[self.pat_fig[i]] for i in range(len(self.pat_fig))}
         return loaders, mapdic, self.data, self.postdata
 
-class SurvNet(torch.nn.Module):
-    def __init__(self, savedmodel, layer, p):
-        super(SurvNet, self).__init__()
-        self.prenet = torch.load(savedmodel).module
-        # res = next(self.prenet.children())
-        # res.fc = torch.nn.Identity()
-        self.prenet.classifier = torch.nn.Sequential(OrderedDict([
-            ('fc1', torch.nn.Linear(1024, layer, bias = True)),
-            ('th1', torch.nn.Tanh()),
-            ('dr1', torch.nn.Dropout(p)),
-            ('fc2', torch.nn.Linear(layer, 1, bias = True)),
-            ('th2', torch.nn.Tanh())
-            ]))
-
-    def fc(self, x):
-        x = self.fc1(x)
-        x = self.th1(x)
-        x = self.dr1(x)
-        x = self.fc2(x)
-        x = self.th2(x)
-        return x
-
-    def forward(self, x):
-        x = self.prenet(x)
-        # x = self.fc(x)
-        return x
-
 class Train(object):
-    def __init__(self, savedmodel, savedmodel2 = None, h5path = None, infopath = None, lr = 1e-4, lr2 = None, batch_size = 64, epochs = 20, layer = 100, p = 0, weight_decay = 5e-4, optim = "SGD", lr_decay = -1, gpus = [0], lrstep = 100, cbstep = 10, figpath = None, mission = 'Surv', ifprint = True, ifcombine = False):
+    def __init__(self, savedmodel, savedmodel2 = None, h5path = None, infopath = None, lr = 1e-4, lr2 = None, batch_size = 64, epochs = 20, layer = 100, p = 0, weight_decay = 5e-4, optim = "SGD", lr_decay = -1, gpus = [0], lrstep = 100, cbstep = 10, figpath = None, mission = 'Surv', ifprint = True, fold = 4, k = 0):
         self.savedmodel = savedmodel
         self.savedmodel2 = savedmodel2
         self.layer = layer
@@ -130,9 +111,11 @@ class Train(object):
         self.weight_decay = weight_decay
         self.gpus = gpus
         self.mission = mission
+        self.fold = fold
+        self.k = k
         self.net = self.__load_net()
         self.loss = SurvLoss()
-        self.loaders, self.mapdic, self.data, self.postdata = Data(h5path).load(batch_size, ifcombine)
+        self.loaders, self.mapdic, self.data, self.postdata = Data(h5path, self.fold, self.k).load(batch_size)
         self.lr = lr
         self.lr2 = lr2 if lr != None else lr
         self.lr_decay = lr_decay
@@ -144,12 +127,7 @@ class Train(object):
         self.ifprint = ifprint
 
     def __get_opt(self):
-        if self.mission in ('Surv', 'FullTrain1FC', "FullTrain2FC"):
-            if self.optim == "Ada":
-                return torch.optim.Adamax(self.net.parameters(), lr = self.lr, weight_decay = self.weight_decay)
-            elif self.optim == "SGD":
-                return torch.optim.SGD(self.net.parameters(), lr = self.lr, momentum = 0.9, nesterov = True, weight_decay = self.weight_decay)
-        elif self.mission == 'ClassSurv':
+        if self.mission == 'ClassSurv':
             if self.optim == "Ada":
                 return torch.optim.Adamax([
                     {'params': self.net.module.prenet.parameters(), 'lr': self.lr, "weight_decay": self.weight_decay},
@@ -164,6 +142,8 @@ class Train(object):
                 opt1 = torch.optim.Adamax(self.net.module.prenet.parameters(), self.lr, weight_decay = self.weight_decay)
                 opt2 = torch.optim.SGD(self.net.module.postnet.parameters(), lr = self.lr2, weight_decay = self.weight_decay, momentum = 0.9, nesterov = True)
                 return opt1, opt2
+        else:
+            raise NotImplementedError("Not supported mission")
 
     def __lr_step(self, i):
         if self.lr_decay != -1:
@@ -173,15 +153,7 @@ class Train(object):
         self.opt = self.__get_opt()
 
     def __load_net(self):
-        if self.mission == 'Surv':
-            net = torch.nn.Sequential(OrderedDict([
-                ('fc1', torch.nn.Linear(1024, self.layer, bias = True)),
-                ('dr1', torch.nn.Dropout(self.p)),
-                ('th1', torch.nn.Tanh()),
-                ('fc2', torch.nn.Linear(self.layer, 1, bias = True)),
-                ('th2', torch.nn.Tanh())
-            ]))
-        elif self.mission == "ClassSurv":
+        if self.mission == "ClassSurv":
             prenet = torch.load(self.savedmodel).module
             prenet.classifier = torch.nn.Identity()
             postnet = torch.load(self.savedmodel2).module
@@ -191,23 +163,8 @@ class Train(object):
             ]))
             for p in net.parameters():
                 p.requires_grad = True
-        elif self.mission == "FullTrain1FC":
-            net = torchvision.models.resnet18(pretrained = True)
-            net.fc = torch.nn.Sequential(OrderedDict([
-                ('dr1', torch.nn.Dropout(self.p)),
-                ('fc1', torch.nn.Linear(512, 1)),
-                ('th1', torch.nn.Tanh())
-            ]))
-        elif self.mission == "FullTrain2FC":
-            net = torchvision.models.resnet18(pretrained = True)
-            net.fc = torch.nn.Sequential(OrderedDict([
-                ('dr1', torch.nn.Dropout(self.p)),
-                ('fc1', torch.nn.Linear(512, self.layer)),
-                ('th1', torch.nn.Tanh()),
-                ('dr2', torch.nn.Dropout(self.p)),
-                ('fc2', torch.nn.Linear(self.layer, 1)),
-                ('th2', torch.nn.Tanh())
-            ]))
+        else:
+            raise NotImplementedError("Not supported mission")
         net = torch.nn.DataParallel(net, device_ids = self.gpus)
         net = net.cuda()
         return net
@@ -228,10 +185,7 @@ class Train(object):
         return x if istrain else y
 
     def __get_x(self, pat):
-        if self.mission != 'Surv':
-            return self.data[self.mapdic[int(pat)]]
-        else:
-            return self.postdata[self.mapdic[int(pat)]]
+        return self.data[self.mapdic[int(pat)]]
 
     def __call_back(self, i, ifprint = True):
         if i % self.cbstep != 0:
@@ -256,22 +210,19 @@ class Train(object):
     def train(self):
         for i in range(self.epochs):
             for pats, y in self.loaders['train']:
-                x = self.__get_instance(pats, True)
-                y = y.cuda()
+                x = self.__get_instance(pats, True); y = y.cuda()
                 self.net.train()
                 if self.optim != 'mix':
                     self.opt.zero_grad()
                 else:
-                    for opt in self.opt:
-                        opt.zero_grad()
+                    for opt in self.opt: opt.zero_grad()
                 yhat = self.net(x)
                 cost = self.loss(yhat, y)
                 cost.backward()
-                if self.optim != 'mix':
+                if self.optim != 'mix': 
                     self.opt.step()
-                else:
-                    for opt in self.opt:
-                        opt.step()
+                else: 
+                    for opt in self.opt: opt.step()
             cis = self.__call_back(i, self.ifprint)
             self.__lr_step(i)
         torch.save(self.net, modelpath)
@@ -280,14 +231,11 @@ class Train(object):
 if __name__ == "__main__":
     global modelpath; global plotpath; global matpath; global outfile
     modelpath, plotpath, matpath, outfile = sys.argv[1:5]
-
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
     params = {
-        # "savedmodel": os.path.join(root, "ImageProcessing/survival_analysis/_models/success.Nov.02_08:26.model"),
-        "savedmodel": os.path.join(root, "ImageProcessing/stain_classification/_models/success.Nov.02_22:27.model"),
+        "savedmodel": os.path.join(root, "ImageProcessing/stain_classification/_models/2.Nov.05_09:31.model"),
         "savedmodel2": os.path.join(root, "ImageProcessing/survival_analysis/_models/623.03_14:27.model"),
         "h5path": os.path.join(root, "ImageProcessing/survival_analysis/_data/compiled.h5"),
-        # "infopath": os.path.join(root,"ImageProcessing/survival_analysis/_data/merged.csv"),
-        # "figpath": os.path.join(root, "ImageProcessing/stain_classification/_data/subsets"),
         "lr": 1e-7, # for resnet part
         "lr2": 5e-7, # for fc part
         "batch_size": 32,
@@ -295,13 +243,12 @@ if __name__ == "__main__":
         "gpus": [0, 1],
         "cbstep": 1,
         "lr_decay": 7.754e-4,
-        # "layer": 128,
-        # "p": 0.8,
         "optim": "mix",
         "weight_decay": 6e-3,
-        "mission": "ClassSurv", # Surv, ClassSurv, FullTrain1FC, FullTrain2FC
+        "mission": "ClassSurv",
         "ifprint": True,
-        "ifcombine": True
+        "fold": 4,
+        "k": 0
     }
     for key, value in params.items():
         print_to_out(key, ":", value)
