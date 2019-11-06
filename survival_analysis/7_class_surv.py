af5622f7e436f32cab1a2311b9879a48671d9926
CarnoZhao <273806108@qq.com>
2019-11-06 19:09:09 +0800

    add combine training

--- survival_analysis/7_class_surv.py
+++ survival_analysis/7_class_surv.py
@@ -67,7 +67,7 @@ class SurvDataset(torch.utils.data.Dataset):
         return self.length
       
 class Data(object):
-    def __init__(self, h5path, fold = 4, k = 0):
+    def __init__(self, h5path, fold = 4, k = 0, ifcombine = False):
         h5 = h5py.File(h5path, 'r')
         self.set_pat = h5['set_pat'][:]
         self.pat_fig = h5['pat_fig'][:]
@@ -75,35 +75,39 @@ class Data(object):
         self.label = h5['label'][:]
         self.data = h5['data']
         self.postdata = h5['postdata'][:]
-        self.names = ['train', 'val', 'test']
         self.fold = fold
         self.k = k
+        self.ifcombine = ifcombine
 
     def __datasets_creater(self):
-        train_val = np.arange(len(self.set_pat))[self.set_pat == 0]
-        inf = int(self.k * len(train_val) / self.fold)
-        sup = int((self.k + 1) * len(train_val) / self.fold)
-        validx = np.arange(len(train_val))
-        validx = np.bitwise_and(np.greater_equal(validx, inf), np.less(validx, sup))
-        train = train_val[np.bitwise_not(validx)]
-        val = train_val[validx]
-        test = np.arange(len(self.set_pat))[self.set_pat == 1]
         datasets = {}
-        datasets['train'] = SurvDataset(train, self.label)
-        datasets['val'] = SurvDataset(val, self.label)
-        datasets['test'] = SurvDataset(test, self.label)
-        self.k += 1
+        train_val = np.arange(len(self.set_pat))[self.set_pat == 0]
+        test = np.arange(len(self.set_pat))[self.set_pat == 1]
+        if self.ifcombine:
+            datasets['train'] = SurvDataset(train_val, self.label)
+            datasets['test'] = SurvDataset(test, self.label)
+        else:
+            inf = int(self.k * len(train_val) / self.fold)
+            sup = int((self.k + 1) * len(train_val) / self.fold)
+            validx = np.arange(len(train_val))
+            validx = np.bitwise_and(np.greater_equal(validx, inf), np.less(validx, sup))
+            train = train_val[np.bitwise_not(validx)]
+            val = train_val[validx]
+            datasets['train'] = SurvDataset(train, self.label)
+            datasets['val'] = SurvDataset(val, self.label)
+            datasets['test'] = SurvDataset(test, self.label)
+            self.k += 1
         return datasets
 
     def load(self, batch_size, k = None):
         if k is not None: self.k = k
         datasets = self.__datasets_creater()
-        loaders = {name: DataLoader(datasets[name], batch_size = batch_size if name == 'train' else 1, shuffle = name == 'train', drop_last = name == 'train') for name in self.names}
+        loaders = {name: DataLoader(datasets[name], batch_size = batch_size if name == 'train' else 1, shuffle = name == 'train', drop_last = name == 'train') for name in datasets}
         mapdic = {i: np.arange(len(self.pat_fig[i]))[self.pat_fig[i]] for i in range(len(self.pat_fig))}
         return loaders, mapdic, self.data, self.postdata
 
 class Train(object):
-    def __init__(self, savedmodel, savedmodel2 = None, h5path = None, infopath = None, lr = 1e-4, lr2 = None, batch_size = 64, epochs = 20, layer = 100, p = 0, weight_decay = 5e-4, optim = "SGD", lr_decay = -1, gpus = [0], lrstep = 100, cbstep = 10, figpath = None, mission = 'Surv', ifprint = True, fold = 4, k = 0):
+    def __init__(self, savedmodel, savedmodel2 = None, h5path = None, infopath = None, lr = 1e-4, lr2 = None, batch_size = 64, epochs = 20, layer = 100, p = 0, weight_decay = 5e-4, optim = "SGD", lr_decay = -1, gpus = [0], lrstep = 100, cbstep = 10, figpath = None, mission = 'Surv', ifprint = True, fold = 4, k = 0, ifcombine = False):
         self.savedmodel = savedmodel
         self.savedmodel2 = savedmodel2
         self.layer = layer
@@ -115,7 +119,7 @@ class Train(object):
         self.k = k
         self.net = self.__load_net()
         self.loss = SurvLoss()
-        self.loaders, self.mapdic, self.data, self.postdata = Data(h5path, self.fold, self.k).load(batch_size)
+        self.loaders, self.mapdic, self.data, self.postdata = Data(h5path, self.fold, self.k, ifcombine).load(batch_size)
         self.lr = lr
         self.lr2 = lr2 if lr != None else lr
         self.lr_decay = lr_decay
@@ -155,7 +159,7 @@ class Train(object):
     def __load_net(self):
         if self.mission == "ClassSurv":
             prenet = torch.load(self.savedmodel).module
-            prenet.classifier = torch.nn.Identity()
+            prenet.fc = torch.nn.Identity()
             postnet = torch.load(self.savedmodel2).module
             net = torch.nn.Sequential(OrderedDict([
                 ('prenet', prenet),
@@ -234,21 +238,22 @@ if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
     params = {
         "savedmodel": os.path.join(root, "ImageProcessing/stain_classification/_models/2.Nov.05_09:31.model"),
-        "savedmodel2": os.path.join(root, "ImageProcessing/survival_analysis/_models/623.03_14:27.model"),
+        "savedmodel2": os.path.join(root, "ImageProcessing/survival_analysis/_models/287.05_20:40.model"),
         "h5path": os.path.join(root, "ImageProcessing/survival_analysis/_data/compiled.h5"),
-        "lr": 1e-7, # for resnet part
-        "lr2": 5e-7, # for fc part
+        "lr": 8e-8, # for resnet part
+        "lr2": 8e-8, # for fc part
         "batch_size": 32,
-        "epochs": 200,
+        "epochs": 100,
         "gpus": [0, 1],
         "cbstep": 1,
-        "lr_decay": 7.754e-4,
-        "optim": "mix",
-        "weight_decay": 6e-3,
+        "lr_decay": 5.6e-6,
+        "optim": "Ada",
+        "weight_decay": 1e-2,
         "mission": "ClassSurv",
         "ifprint": True,
         "fold": 4,
-        "k": 0
+        "k": 2,
+        "ifcombine": True
     }
     for key, value in params.items():
         print_to_out(key, ":", value)
